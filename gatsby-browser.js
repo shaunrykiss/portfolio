2facdf6413e763a6cd104f371a3f74c1d27e3aa1
Amanda Silberberg <amanda.silberberg@gmail.com>
2021-01-30 15:47:14 -0500

    added font preloader

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,5 @@
 import './src/styles.scss';
+
+require('typeface-oswald');
+require('typeface-montserrat');
+require('typeface-raleway');
--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@
  */
 
 module.exports = {
-  /* Your site config here */
   siteMetadata: {
     title: "Shaun Rykiss | Editor",
   },
@@ -17,21 +16,10 @@ module.exports = {
         accessToken: `2yh9oybjs66UpOgClcnce9ZLCCgQZiEeVR8d1egECNc`,
       },
     },
-    {
-      resolve: "gatsby-plugin-web-font-loader",
-      options: {
-        google: {
-          families: [
-            "Montserrat:300,500,700",
-            "Oswald:200,300,400",
-            "Raleway:300,500",
-          ],
-        },
-      },
-    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-preload-fonts`,
   ],
 }
